8d8b42d28328a27b21a37cb5ea1e31e3dc354393
Kumeda Shintaro <pii.k2.bys@gmail.com>
2021-01-16 17:50:47 +0900

    getリクエスト実装、permission追加

--- app/src/main/java/kumeda/cookingrecord/MainActivity.kt
+++ app/src/main/java/kumeda/cookingrecord/MainActivity.kt
@@ -2,10 +2,25 @@ package kumeda.cookingrecord
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kumeda.cookingrecord.repository.Repository
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+        viewModel.getPost()
+        viewModel.myResponse.observe(this, Observer { response ->
+            Log.d("Response", response.cooking_records.toString())
+        })
     }
 }
